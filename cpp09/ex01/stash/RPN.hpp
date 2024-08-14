1b8bea719c607971aae90efa08dc7f916bee5245
Thomas Lam <thomas.lam0411@gmail.com>
2024-08-14 18:00:09 +0200

    ENDED

---
+++ cpp09/ex01/stash/RPN.hpp
@@ -0,0 +1,21 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <deque>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+
+std::deque<std::string> parseToDeque(char* input);
+int checkDeque(std::deque<std::string> deque);
+bool isStringDigit(std::string str);
+bool isStringOperator(std::string str);
+void operate(std::deque<int>& numq, std::deque<char>& opq);
+void RPN(std::deque<std::string> deque);
+
+void printDeque(std::deque<std::string> deque);
+void printDequeInt(std::deque<int> deque);
+
+#endif
---
+++ cpp09/ex01/stash/main.cpp
@@ -0,0 +1,30 @@
+#include "RPN.hpp"
+
+// peut etre fait differemment. Creer une deque (ou stack). 
+// Tant qu'on trouve des nombres, les ajouter a la stack, puis appliquer le / les tokens'
+// puis les retirer de la stack, et ajouter le resultat until end;
+
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        std::cout << "Invalid number of arguments, RPN only takes a string as input.\nLaunch like this : ./RPN \"X Y + Z * W -...\"" << std::endl;
+        return -1;
+    }
+
+    std::deque<std::string> deque = parseToDeque(argv[1]);
+
+    // printDeque(deque);
+
+    if (!checkDeque(deque)) {
+        std::cout << "Invalid input" << std::endl;
+        return -1;
+    }
+    else {
+		RPN(deque);
+	}
+
+	
+
+    return 0;
+}
